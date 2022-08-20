e5f322a7560e35841e755024d843c9d1732abb3c
Farhan Reynaldo <farhanreynaldo@gmail.com>
2022-08-20 20:05:08 +0700

    create compare with bootstrap and delta method

---
+++ abby/compare.py
@@ -0,0 +1,93 @@
+"""Compare module."""
+import numpy as np
+import scipy
+from tqdm import tqdm
+
+
+def compare_bootstrap(
+    conversions_ctrl,
+    sessions_ctrl,
+    conversions_exp,
+    sessions_exp,
+    n_bootstrap: int = 10_000,
+):
+    n_users_a, n_users_b = len(conversions_ctrl), len(conversions_exp)
+    n_users = n_users_a + n_users_b
+    bs_observed = []
+
+    for _ in tqdm(range(n_bootstrap)):
+        conversion = np.hstack((conversions_ctrl, conversions_exp))
+        session = np.hstack((sessions_ctrl, sessions_exp))
+
+        assignments = np.random.choice(n_users, n_users, replace=True)
+        ctrl_idxs = assignments[: int(n_users / 2)]
+        test_idxs = assignments[int(n_users / 2) :]
+
+        bs_sessions_ctrl = session[ctrl_idxs]
+        bs_sessions_exp = session[test_idxs]
+        bs_conversions_ctrl = conversion[ctrl_idxs]
+        bs_conversions_exp = conversion[test_idxs]
+
+        bs_observed.append(
+            bs_conversions_exp.sum() / bs_sessions_exp.sum()
+            - bs_conversions_ctrl.sum() / bs_sessions_ctrl.sum()
+        )
+
+    observed_diffs = (
+        conversions_exp.sum() / sessions_exp.sum()
+        - conversions_ctrl.sum() / sessions_ctrl.sum()
+    )
+    p_values = 2 * (
+        1 - (np.abs(observed_diffs) > np.array(bs_observed)).mean()
+    )
+    return p_values
+
+
+def compare_delta(
+    conversions_ctrl,
+    sessions_ctrl,
+    conversions_exp,
+    sessions_exp,
+):
+    n_users_a, n_users_b = len(conversions_ctrl), len(conversions_exp)
+
+    mean_sessions_ctrl = sessions_ctrl.mean()
+    mean_conversions_ctrl = conversions_ctrl.mean()
+    var_sessions_ctrl = sessions_ctrl.var()
+    var_conversions_ctrl = conversions_ctrl.var()
+
+    mean_sessions_exp = sessions_exp.mean()
+    mean_conversions_exp = conversions_exp.mean()
+    var_sessions_exp = sessions_exp.var()
+    var_conversions_exp = conversions_exp.var()
+
+    cov_ctrl = (
+        (conversions_ctrl - mean_conversions_ctrl)
+        * (sessions_ctrl - mean_sessions_ctrl)
+    ).mean()
+    cov_exp = (
+        (conversions_exp - mean_conversions_exp)
+        * (sessions_exp - mean_sessions_exp)
+    ).mean()
+
+    var_ctrl = (
+        var_conversions_ctrl / mean_sessions_ctrl ** 2
+        + var_sessions_ctrl
+        * mean_conversions_ctrl ** 2
+        / mean_sessions_ctrl ** 4
+        - 2 * mean_conversions_ctrl / mean_sessions_ctrl ** 3 * cov_ctrl
+    )
+    var_exp = (
+        var_conversions_exp / mean_sessions_exp ** 2
+        + var_sessions_exp * mean_conversions_exp ** 2 / mean_sessions_exp ** 4
+        - 2 * mean_conversions_exp / mean_sessions_exp ** 3 * cov_exp
+    )
+
+    cvrs_ctrl = conversions_ctrl.sum() / sessions_ctrl.sum()
+    cvrs_exp = conversions_exp.sum() / sessions_exp.sum()
+
+    z_scores = np.abs(cvrs_exp - cvrs_ctrl) / np.sqrt(
+        var_ctrl / n_users_a + var_exp / n_users_b
+    )
+    p_values = 2 * (1 - scipy.stats.norm(loc=0, scale=1).cdf(z_scores))
+    return cvrs_ctrl, cvrs_exp, p_values
